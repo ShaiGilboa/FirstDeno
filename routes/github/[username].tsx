e66b7b7c674df22a22c0fa2a633b23a61de90121
Shai Gilboa <shizel@gmail.com>
2022-07-30 22:56:50 -0400

    change sgithub endpoint

--- routes/github/[username].tsx
+++ routes/github/[username].tsx
@@ -27,8 +27,9 @@ export default function Page({ data }: PageProps<User | null>) {
 
   return (
     <div>
-      <img src={data.avatar_url} width={64} height={64} />
+      <h1>GitHub Username Getter</h1>
       <h1>{data.name}</h1>
+      <img src={data.avatar_url} width={64} height={64} />
       <p>{data.login}</p>
     </div>
   );
